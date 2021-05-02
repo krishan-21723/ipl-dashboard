8ee756d78abb4ce0e8b748c1de4b8ba1080c33fb
krishan21723 <kchhimpa@noon.com>
2021-05-02 14:57:26 +0530

    Added match api

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 function App() { 
   return (
     <div className="App">
-     <TeamPage/>
+      <Router>
+        <Switch>
+        <Route path='/teams/:teamName/matches/:year'>
+            <MatchPage/>
+          </Route>
+          <Route path='/teams/:teamName'>
+            <TeamPage/>
+        </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
